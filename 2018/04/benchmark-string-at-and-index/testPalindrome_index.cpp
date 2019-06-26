08b8b25c4cd9008ac4cdc7e141f4972752e18512
Nan Xiao <nan@chinadtrace.org>
2019-06-26 17:45:37 +0800

    Add benchmark-string-at-and-index

---
+++ 2018/04/benchmark-string-at-and-index/testPalindrome_index.cpp
@@ -0,0 +1,25 @@
+#include <string>
+
+bool isPalindrome(
+    std::string& s,
+    std::string::size_type start,
+    std::string::size_type end) {
+
+        auto count = (end - start + 1) / 2;
+        for (std::string::size_type i = 0; i < count; i++) {
+            if (s[start] != s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+}
+
+int main() {
+        std::string s(1'000'000'000, 'a');
+
+        isPalindrome(s, 0, s.size() - 1);
+        return 0;
+}
