511e90179e2f983a9f0be800a5bc30f95f7e94b7
Nan Xiao <nan@chinadtrace.org>
2019-08-20 09:57:13 +0800

    Add pcap_utilities

---
+++ 2019/08/pcap_utilities/ooo.c
@@ -0,0 +1,69 @@
+#include <pcap/pcap.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main()
+{
+	const char *src_file = "./original.pcap";
+	const char *dst_file = "./google-out-of-order.pcap";
+	pcap_t *src_handle = NULL;
+	pcap_t *dst_handle = NULL;
+	int src_index = 6;
+	int dst_index = 8;
+
+	char err[PCAP_ERRBUF_SIZE];
+	src_handle = pcap_open_offline(src_file, err);
+	if (src_handle == NULL) {
+		printf("Open %s failed: %s\n", src_file, err);
+		return EXIT_FAILURE;
+	}
+
+	dst_handle = pcap_open_dead(DLT_EN10MB, 262144);
+	pcap_dumper_t *dst_dump = pcap_dump_open(dst_handle, dst_file);
+	if (dst_dump == NULL) {
+		printf("pcap_dump_open error: %s\n", pcap_geterr(dst_handle));
+		return EXIT_FAILURE;
+	}
+
+	while (1)
+	{
+		struct pcap_pkthdr *hdr = NULL;
+		static struct pcap_pkthdr src_index_hdr;
+
+		const u_char *data = NULL;
+		static u_char *src_index_data = NULL;
+
+		static int count = 0;
+
+		int ret = pcap_next_ex(src_handle, &hdr, &data);
+		if (ret == 1) {
+			if (++count == src_index) {
+				memcpy(&src_index_hdr, hdr, sizeof(struct pcap_pkthdr));
+				src_index_data = malloc(hdr->caplen);
+				if (src_index_data == NULL) {
+					printf("malloc failed\n");
+					return EXIT_FAILURE;
+				}
+				memcpy(src_index_data, data, hdr->caplen);
+			} else {
+				pcap_dump((u_char *)dst_dump, hdr, data);
+				if (count == dst_index) {
+					pcap_dump((u_char *)dst_dump, &src_index_hdr, src_index_data);
+				}
+			}
+		} else if (ret == PCAP_ERROR_BREAK) {
+			break;
+		} else {
+			printf("pcap_next_ex error: %s\n", pcap_geterr(src_handle));
+			return EXIT_FAILURE;
+		}
+	}
+
+	pcap_dump_close(dst_dump);
+	pcap_close(dst_handle);
+	pcap_close(src_handle);
+
+	return EXIT_SUCCESS;
+}
